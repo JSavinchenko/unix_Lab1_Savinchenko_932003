9fc3f40a666f321c1a489da34517371c2b9d7fe9
Juls <111127701+JSavinchenko@users.noreply.github.com>
2023-11-01 13:59:59 +0500

    Add files via upload

---
+++ Laba1/unixTask1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+
+sourceFile=$1
+
+if [ -z "$sourceFile" ]; then
+  echo "enter cpp filename"
+  exit 1
+fi
+
+outputFile="$(grep -i "Output:" "$sourceFile" | cut -d ':' -f2- | tr -d '[:space:]/')"
+
+tempDirectory=$(mktemp -d)
+thisDirectory=$(pwd)
+
+cp "$sourceFile" "$tempDirectory"
+cd "$tempDirectory"
+
+if [ -z "$outputFile" ]; then
+  echo "Name for output file is empty in the .cpp file."
+  exit 1
+fi
+
+g++ "$sourceFile" -o "$outputFile"
+mv "$outputFile" "$thisDirectory"
+cd "$thisDirectory"
+
+delete_dir(){
+echo "Temporary directory was deleted"
+rm -rf $tempDirectory
+}
+
+trap delete_dir EXIT HUP INT QUIT PIPE TERM
+
+echo "Successful compilation. Output: $outputFile"
+exit 0
